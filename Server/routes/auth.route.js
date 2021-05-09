b8b9c41feb49a916e3a3785895a55411f0ed1d83
alexandrmaliovaniy <alexandrmaliovaniy@gmail.com>
2021-05-09 23:24:04 +0300

    server registration

---
+++ Server/routes/auth.route.js
@@ -0,0 +1,52 @@
+const {Router} = require('express');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const router = Router();
+
+
+
+// /api/auth/login //
+
+router.post("/login", async(req, res) => {
+
+});
+
+// /api/auth/registration //
+
+router.post("/registration", async(req, res) => {
+    try {
+        const {email, username, password} = req.body;
+
+        const uniqueMail = await User.findOne({email});
+        if (uniqueMail) return res.status(400).json({email: "This email is already registered!"});
+
+        const uniqueUsername = await User.findOne({username});
+        if (uniqueUsername) return res.status(400).json({username: "This name is already taken"});
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const newUser = new User({
+            email,
+            username,
+            password: hashedPassword
+        });
+        await newUser.save();
+
+        const token = jwt.sign(
+            {
+                id: newUser.id,
+                username: newUser.username
+            },
+            config.get('secret'),
+            {expiresIn: ''}
+        )
+
+        res.status(200).json({token, id: newUser.id, username: newUser.username});
+        
+    } catch(e) {
+        throw e
+    }
+
+}); 
