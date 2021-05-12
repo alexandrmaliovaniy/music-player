14f044132e343a2616099e442c372807fe4cc619
alexandrmaliovaniy <alexandrmaliovaniy@gmail.com>
2021-05-12 20:43:33 +0300

    sidebar menu

--- WebClient/src/components/Sidebar/Sidebar.js
+++ WebClient/src/components/Sidebar/Sidebar.js
@@ -1,8 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faHome, faPlus, faSearch } from "@fortawesome/free-solid-svg-icons";
 import './Sidebar.css';
+
+
 const SideBar = () => {
     return (
         <div className="Sidebar">
-            
+            <ul className="navBar">
+                <li className="menuItem activeMenuItem">
+                    <FontAwesomeIcon icon={faHome} className="menuIcon" />
+                    Home
+                </li>
+                <li className="menuItem">
+                    <FontAwesomeIcon icon={faSearch} className="menuIcon" />
+                    Search
+                </li>
+            </ul>
+
+            <ul className="userOptions">
+                <li className="userOptionItem activeUserOptionItem">
+                    <FontAwesomeIcon icon={faPlus} className="userOptionIcon" />
+                    create new playlist
+                </li>
+                <li className="userOptionItem">
+                    <FontAwesomeIcon icon={faHeart} className="userOptionIcon" />
+                    favorite songs
+                </li>
+            </ul>
+
+            <div className="playlists">
+                
+            </div>
         </div>
     )
 }
