e4c4fc49ae9d446f81d4f11ef8aaf94bfea89a25
alexandrmaliovaniy <alexandrmaliovaniy@gmail.com>
2021-05-17 21:10:18 +0300

    current page fix

--- WebClient/src/pages/PlaylistPage.js
+++ WebClient/src/pages/PlaylistPage.js
@@ -1,9 +1,18 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import Playlist from '../components/Playlist/Playlist';
 import PlaylistBanner from '../components/Playlist/PlaylistBanner';
+import {CurrentPageContext} from '../context/CurrentPageContext';
 import './PlaylistPage.css';
 
 const PlaylistPage = () => {
+
+    const {setCurrentPage} = useContext(CurrentPageContext);
+    
+    useEffect(() => {
+        setCurrentPage(null);
+    }, [])
+
+
     const [data, setData] = useState({
         image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.smehost.net%2Frcarecordscom-usrcaprod%2Fwp-content%2Fuploads%2F2018%2F01%2FTDG-OUTSIDER-album-artwork-561x561.jpg&f=1&nofb=1",
         name: "This is name of playlist",
