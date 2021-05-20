e78d450e47f579e693437900b1cc03babf95bd62
alexandrmaliovaniy <alexandrmaliovaniy@gmail.com>
2021-05-20 22:45:14 +0300

    save

--- Server/routes/song.routes.js
+++ Server/routes/song.routes.js
@@ -1,6 +1,8 @@
 const {Router} = require('express');
 const bcrypt = require('bcryptjs');
 const config = require('config');
+const {Type} = require('mongoose');
+const mongodb = require('mongodb');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const Song = require('../models/Song');
@@ -8,10 +10,18 @@ const router = Router();
 
 
 router.get('/:id', async(req, res) => {
-    const songId = req.params.id;
+    const songId = Type.ObjectId(req.params.id);
+
+    res.set('content-type', 'audio/mp3');
+    res.set('accept-ranges', 'bytes');
+
+    const bucket = new mongodb.GridFSBucket()
+
+
+
     const song = await Song.findById(songId);
     if (!song) return res.status(404).json({message: "song not found"});
-    res.json(song);
+    res.json(song.data);
 })
 
 
