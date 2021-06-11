b92353efbc02d32f91ba504361a12a35e81d352a
alexandrmaliovaniy <alexandrmaliovaniy@gmail.com>
2021-06-11 20:19:18 +0300

    username cyrillic support

--- WebClient/src/hooks/formError.hook.js
+++ WebClient/src/hooks/formError.hook.js
@@ -26,7 +26,7 @@ export const useFormError = (errorFields) => {
             Set(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/, input, fieldName, cb);
         },
         Username: (fieldName, input, cb) => {
-            Set(/^[a-zA-Z0-9\s-]+$/, input, fieldName, cb);
+            Set(/^[a-zA-ZА-Яа-я0-9\s-]+$/, input, fieldName, cb);
         },
         Password: (fieldName, input, cb) => {
             Set(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/, input, fieldName, cb);
