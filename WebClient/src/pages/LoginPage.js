74b980c47e4163f188ec88acd95666b8884c631c
alexandrmaliovaniy <alexandrmaliovaniy@gmail.com>
2021-06-14 21:30:34 +0300

    login redirect to home

--- WebClient/src/pages/LoginPage.js
+++ WebClient/src/pages/LoginPage.js
@@ -1,5 +1,5 @@
 import React, {useState, useContext} from 'react';
-
+import {useHistory} from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faEye, faEyeSlash, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { useFormError } from '../hooks/formError.hook';
@@ -9,6 +9,9 @@ import { AuthContext } from '../context/AuthContext';
 import './LoginPage.css';
 import './AuthForm.css';
 const LoginPage = () => {
+
+
+    const history = useHistory();
 
     const {loading, request} = useHttp();
     const {login} = useContext(AuthContext);
@@ -62,6 +65,7 @@ const LoginPage = () => {
                 password: formInput.password
             });
             login(loginData.token, loginData.id, loginData.username);
+            history.push("/");
         } catch(e) {
             setFieldStatus({...fieldStatus, ...e})
         }
